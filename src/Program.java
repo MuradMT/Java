b49c02e425e78b7da766d5c84777a7f7b34692d9
Murad Mammadzade <mammadzade03@gmail.com>
2023-03-04 00:14:39 +0400

    do modifications

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,16 @@
 public class Program {
     public static void main(String [] args){
         System.out.println("Hello World");
+        System.out.print("Hello World\n");
+        System.out.println("Hello World");
+        int x=7;
+        int y=2;
+        System.out.print(x%y);
+        String t="\\";
+        boolean k=true;
+        char ta='9';
 
     }
+
+
 }
